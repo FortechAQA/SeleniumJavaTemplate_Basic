a9d0636046df1375c7bc0e6a26483821deb52d6e
Genescu Raul <raul.genescu@fortech.ro>
2023-07-27 09:15:32 +0300

    first commit

---
+++ src/test/java/TestPageAccess.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class TestPageAccess {
+    public static WebDriver driver;
+    public static final String URL = "https://www.saucedemo.com/";
+
+    @Test
+    public void testPageAccess() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        final String driverChrome = "webdriver.chrome.driver";
+        final String chromeDriverAddress = "C:\\Program Files\\Google\\Chrome\\Application\\chromedriver.exe";
+        chromeOptions.addArguments("--remote-allow-origins=*");
+        chromeOptions.addArguments("--no-sandbox");
+        System.setProperty(driverChrome, chromeDriverAddress);
+
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+        driver.get(URL);
+
+    }
+}
